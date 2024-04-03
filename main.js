78c86775280cc0e018bc62e0d8de45577a3d28b3
ProfTerezinha <127345327+ProfTerezinha@users.noreply.github.com>
2024-04-03 19:36:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,9 @@ for(let i = 0; i < botoes.length; i++){
 const contadores = document.querySelectorAll(".contador");
 let tempoAtual = new Date();
 const tempoObjetivo1 = new Date("2024-05-03T00:00:00");
-let tempoFinal = tempoObjetivo1 - tempoAtual;
+
+function calculaTempo(tempoObjetivo);{
+let tempoFinal = tempoObjetivo - tempoAtual;
 let segundos = Math.floor(tempoFinal / 1000);
 let minutos = Math.floor(segundos / 60);
 let horas = Math.floor(minutos / 60);
@@ -23,5 +25,8 @@ let dias = Math.floor(horas / 24);
 segundos %= 60;
 minutos %= 60;
 horas %= 24;
-contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";    
+} 
+
+contadores[0].textContent = calculaTempo(tempoObetivo1);
 
