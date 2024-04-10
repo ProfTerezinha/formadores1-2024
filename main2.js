cce43fcd7c1fdf14c250bd98693a2dc5293da6d7
ProfTerezinha <127345327+ProfTerezinha@users.noreply.github.com>
2024-04-10 19:39:06 -0300

    fff

---
+++ main2.js
@@ -0,0 +1,5 @@
+const dataAtual = new Date();
+let dataObjetivo = prompt("Data final AAAA-MM-DD");
+dataObjetivo = new Date(dataObjetivo + "T23:59:59");
+let diasQueFaltam =Math.floor((dataObjetivo - dataAtual); /8640000);
+document.querySelector("#dias_restantes").textContent = diasQueFaltam;
